1310f41da53e66057481fec5a60449cb8b957336
Stephan Janssen <sja@devoxx.com>
2019-12-28 09:19:15 +0100

    Show color bar

--- color-detection/run.sh
+++ color-detection/run.sh
@@ -3,5 +3,5 @@
 for i in {0..25}
 do
    filename="samples/segmented$i.jpg"
-   python color.py $filename
+   python show_colors.py -i $filename
 done
---
+++ color-detection/show_colors.py
@@ -0,0 +1,64 @@
+import cv2
+import numpy as np
+import argparse
+import ntpath
+import matplotlib.pyplot as plt
+from sklearn.cluster import KMeans
+
+def find_histogram(clt):
+    """
+    create a histogram with k clusters
+    :param: clt
+    :return:hist
+    """
+    numLabels = np.arange(0, len(np.unique(clt.labels_)) + 1)
+    (hist, _) = np.histogram(clt.labels_, bins=numLabels)
+
+    hist = hist.astype("float")
+    hist /= hist.sum()
+
+    return hist
+
+def plot_colors2(hist, centroids):
+    bar = np.zeros((50, 300, 3), dtype="uint8")
+    startX = 0
+
+    for (percent, color) in zip(hist, centroids):
+        # plot the relative percentage of each cluster
+        endX = startX + (percent * 300)
+        cv2.rectangle(bar, (int(startX), 0), (int(endX), 50),
+                      color.astype("uint8").tolist(), -1)
+        startX = endX
+
+    # return the bar chart
+    return bar
+
+def saveColorBar(bar):
+    head, tail = ntpath.split(args["image"])
+    parts = tail.split(".")
+    output = "{}/{}-colorbar.jpg".format(head, parts[0])
+    print("Color bar stored in %s" %output)
+    cv2.imwrite(output, bar)
+
+
+# construct the argument parse and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--image", required=True, help="path to input image")
+args = vars(ap.parse_args())
+
+img = cv2.imread(args["image"])
+img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+img = img.reshape((img.shape[0] * img.shape[1],3)) #represent as row*column,channel number
+clt = KMeans(n_clusters=3) #cluster number
+clt.fit(img)
+
+hist = find_histogram(clt)
+bar = plot_colors2(hist, clt.cluster_centers_)
+
+saveColorBar(bar)
+
+plt.axis("off")
+plt.imshow(bar)
+plt.show()
+
